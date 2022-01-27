20c45e6a1a0474e3f3a65b26d0f5c2e72b6db1bf
i_kap <iryna.kapelka@gmail.com>
2022-01-27 19:17:44 +0300

    fix - 3.2

--- src/main/java/hogwarts/school/model/Faculty.java
+++ src/main/java/hogwarts/school/model/Faculty.java
@@ -1,11 +1,22 @@
 package hogwarts.school.model;
 
+import nonapi.io.github.classgraph.json.Id;
+
 import java.util.Objects;
 
+//@Entity
 public class Faculty {
+    @Id
+    //@GeneratedValue
     private long id;
     private String name;
     private String color;
+
+    public Faculty (long id, String name, String color){
+        this.id = id;
+        this.name = name;
+        this.color = color;
+    }
 
     public long getId() {
         return id;
@@ -30,7 +41,6 @@ public class Faculty {
     public void setColor(String color) {
         this.color = color;
     }
-
 
     @Override
     public boolean equals(Object other) {
--- src/main/java/hogwarts/school/model/Student.java
+++ src/main/java/hogwarts/school/model/Student.java
@@ -7,6 +7,11 @@ public class Student {
     private String name;
     private int age;
 
+    public Student(Long id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
 
     public Long getId() {
         return id;
